22cffaa58d83a019b47e1d70632cdd1406bb386c
rasm <ralphadriansanmiguel@gmail.com>
2024-08-05 23:28:44 +0800

    Add renderer and animation

--- main.js
+++ main.js
@@ -16,9 +16,24 @@ camera.position.z = 5;
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cube = new THREE.Mesh(geomtry, material);
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+camera.position.z = 5;
+
 // Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// Animation
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate()
 
 
