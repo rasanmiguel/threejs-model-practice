5e3eba7b9f426ba204f0e3ff34be9cf163a0fafa
rasm <ralphadriansanmiguel@gmail.com>
2024-08-05 23:22:14 +0800

    Added code for Mesh

--- main.js
+++ main.js
@@ -10,7 +10,14 @@ const nearClipping = 0.1;
 const farClipping = 1000;
 const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearClipping, farClipping);
 
-// Model = Mesh + Texture
+camera.position.z = 5;
+
+// Mesh = Geometry + Material
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+const cube = new THREE.Mesh(geomtry, material);
+scene.add(cube);
 
 // Renderer
 
